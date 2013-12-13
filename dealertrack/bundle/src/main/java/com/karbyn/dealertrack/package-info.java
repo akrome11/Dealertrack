3d8ce91afa57f78f218a1693b0eebcd4d418684c
Alan <alan@karbyn.com>
2013-12-13 10:56:24 -0500

    This is a test

---
+++ dealertrack/bundle/src/main/java/com/karbyn/dealertrack/package-info.java
@@ -0,0 +1,4 @@
+@Version("1.0.0")
+package com.karbyn.dealertrack;
+
+import aQute.bnd.annotation.Version;
---
+++ dealertrack/bundle/src/test/java/com/karbyn/dealertrack/SimpleUnitTest.java
@@ -0,0 +1,14 @@
+package com.karbyn.dealertrack;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleUnitTest {
+
+    @Test
+    public void someTest() {
+        assertTrue(true);
+    }
+
+}
